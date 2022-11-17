2711d1259f32df1983166d94abe7b227b735b73a
orhanince <ince.orhan@yandex.com>
2022-11-17 15:17:22 +0300

    Implement markTodoComplete, markTodoUnComplete, deleteTodo endpoints and update app name on readme file.

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -2,10 +2,11 @@ const moment = require('moment-timezone');
 const { v4: uuidv4 } = require('uuid');
 const { Todo } = require('./../models');
 const paginationOptionGenerator = require('./../utils/pagination-option-generator');
+const GenericError = require('../utils/generic-error');
 /**
- * Get all todos from database
+ * Get all todos from database.
  */
-async function getAll({ pagination, AUTH }) {
+async function listTodos({ pagination, AUTH }) {
   const options = paginationOptionGenerator({
     pagination,
     likeColumns: ['uuid:todo_id', 'uuid:user_id'],
@@ -30,7 +31,7 @@ async function getAll({ pagination, AUTH }) {
 /**
  * Create todo.
  */
-async function create({ body, AUTH }) {
+async function createTodo({ body, AUTH }) {
   const { title } = body || {};
   const now = moment.utc().toISOString();
   const createTodo = await Todo.create({
@@ -50,7 +51,111 @@ async function create({ body, AUTH }) {
   }
 }
 
+/**
+ * Mark todo as completed.
+ */
+async function markTodoCompleted({ params, AUTH }) {
+  const { todo_id } = params || {};
+  const unCompletedTodoCount = await Todo.count({
+    where: {
+      todo_id: todo_id,
+      user_id: AUTH.user_id,
+      is_completed: 0,
+    },
+  });
+  if (!unCompletedTodoCount) {
+    throw new GenericError(400, 'Todo is not exists', unCompletedTodoCount);
+  }
+  const now = moment.utc().toISOString();
+  const [markTodoCompleted] = await Todo.update(
+    {
+      is_completed: 1,
+      updated_at: now,
+    },
+    {
+      where: {
+        todo_id: todo_id,
+        user_id: AUTH.user_id,
+      },
+    }
+  );
+  return {
+    status: true,
+    data: markTodoCompleted,
+  };
+}
+
+/**
+ * Mark todo as uncompleted.
+ */
+async function markTodoUncompleted({ params, AUTH }) {
+  const { todo_id } = params || {};
+  const completedTodoCount = await Todo.count({
+    where: {
+      todo_id: todo_id,
+      user_id: AUTH.user_id,
+      is_completed: 1,
+    },
+  });
+  if (!completedTodoCount) {
+    throw new GenericError(400, 'Todo is not exists', completedTodoCount);
+  }
+  const now = moment.utc().toISOString();
+  const [markTodoUncompleted] = await Todo.update(
+    {
+      is_completed: 0,
+      updated_at: now,
+    },
+    {
+      where: {
+        todo_id: todo_id,
+        user_id: AUTH.user_id,
+      },
+    }
+  );
+  return {
+    status: true,
+    data: markTodoUncompleted,
+  };
+}
+
+/**
+ * Delete todo.
+ */
+async function deleteTodo({ params, AUTH }) {
+  const { todo_id } = params || {};
+  const foundTodoCount = await Todo.count({
+    where: {
+      todo_id: todo_id,
+      user_id: AUTH.user_id,
+    },
+  });
+  if (!foundTodoCount) {
+    throw new GenericError(400, 'Todo is not exists', foundTodoCount);
+  }
+  const now = moment.utc().toISOString();
+  const [deleteTodo] = await Todo.update(
+    {
+      status: 0,
+      deleted_at: now,
+    },
+    {
+      where: {
+        todo_id: todo_id,
+        user_id: AUTH.user_id,
+      },
+    }
+  );
+  return {
+    status: true,
+    data: deleteTodo,
+  };
+}
+
 module.exports = {
-  getAll,
-  create,
+  listTodos,
+  createTodo,
+  markTodoCompleted,
+  markTodoUncompleted,
+  deleteTodo,
 };
